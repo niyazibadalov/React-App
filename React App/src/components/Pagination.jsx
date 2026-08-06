e7381ab55a5f0255d5316214b13eaf048661b57e
Niyazi <niyazibedelov1@gmail.com>
2026-08-06 13:59:36 +0400

    Pagination

--- React App/src/components/Pagination.jsx
+++ React App/src/components/Pagination.jsx
@@ -1,25 +1,24 @@
 export default function Pagination({ currentPage, totalPages, onPageChange }) {
-  if (totalPages <= 1) return null;
-
   return (
     <div className="pagination">
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className="btn-page"
       >
         Əvvəlki
       </button>
-      <span className="page-info">
+
+      <span>
         Səhifə {currentPage} / {totalPages}
       </span>
+
       <button
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
-        className="btn-page"
       >
         Növbəti
       </button>
     </div>
   );
+
 }
